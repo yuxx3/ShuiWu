925bd9ee74f45fde27367d044e65faf7575b6878
yuxx3 <yuxx3@yonyou.com>
2016-08-29 13:36:33 +0800

    构建路径
    Change-Id: I67f2bf0429c717b3ac46b1d2d7fe9b636a3dfd45
    Signed-off-by: yuxx3 <yuxx3@yonyou.com>

--- ShuiWu/mmppac/src/private/nc/impl/mmppac/orders/ProductOrderImpl.java
+++ ShuiWu/mmppac/src/private/nc/impl/mmppac/orders/ProductOrderImpl.java
@@ -11,7 +11,7 @@ public class ProductOrderImpl implements IProductOrder {
 
 	@Override
 	public Map<String, Object> getOrders(String code) throws Exception {
-		Map<String, Object> result = new HashMap<String, Object>();
+		Map result = new HashMap();
 		String sql = "select a.vbillcode,b.name,a.dbilldate from mm_pmo a,org_factory b where a.pk_org = b.pk_factory and a.dr = 0 order by a.dbilldate desc";
 		BaseDAO dao = new BaseDAO();
 		Object list = dao.executeQuery(sql, new MapListProcessor());
--- ShuiWu/uapbd/src/client/nc/ui/ambd/action/batch/AMBatchRefreshAction.java
+++ ShuiWu/uapbd/src/client/nc/ui/ambd/action/batch/AMBatchRefreshAction.java
@@ -1,13 +1,12 @@
 package nc.ui.ambd.action.batch;
 
 import java.awt.event.ActionEvent;
-import java.util.UUID;
+import java.util.Map;
 
 import nc.bs.framework.common.NCLocator;
 import nc.itf.ampub.upload.IUpLoadFileForAM;
-import nc.itf.uap.IVOPersistence;
+import nc.itf.mmppac.orders.IProductOrder;
 import nc.ui.uif2.actions.batch.BatchRefreshAction;
-import nc.vo.ampub.upload.UpLoadVO;
 
 public class AMBatchRefreshAction extends BatchRefreshAction {
 
@@ -16,7 +15,7 @@ public class AMBatchRefreshAction extends BatchRefreshAction {
 	@Override
 	public void doAction(ActionEvent e) throws Exception {
 		super.doAction(e);
-		UpLoadVO vo = new UpLoadVO();
+		/*UpLoadVO vo = new UpLoadVO();
 		//getService().saveUpLoadFile(vo);
 		UUID id = UUID.randomUUID();
 		vo.setPk_upload(id.toString());
@@ -24,14 +23,16 @@ public class AMBatchRefreshAction extends BatchRefreshAction {
 		vo.setCreatetime("2016-08-16");
 		vo.setImgpath("c://pictures/test");
 		//getUapService().insertVO(vo);
-		getService().saveUpLoadFile(vo);
+		getService().saveUpLoadFile(vo);*/
+		Map<String,Object> result = this.getOrderService().getOrders("");
 	}
 	
 	private IUpLoadFileForAM getService(){
 		return NCLocator.getInstance().lookup(IUpLoadFileForAM.class);
 	}
 	
-	private IVOPersistence getUapService(){
-		return NCLocator.getInstance().lookup(IVOPersistence.class);
+	private IProductOrder getOrderService(){
+		return NCLocator.getInstance().lookup(IProductOrder.class);
 	}
+
 }
